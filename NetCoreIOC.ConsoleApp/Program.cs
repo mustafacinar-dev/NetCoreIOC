fa3fc48ebdd718fd2bc43e0c0e2e65f629cbc2ff
Mustafa ÇINAR <mustafa.cinar7@hotmail.com>
2021-05-02 21:56:30 +0300

    Initial commit

---
+++ NetCoreIOC.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreIOC.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
