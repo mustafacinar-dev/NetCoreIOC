6f024be425a8b47d8af295f1eb53111ae8607961
Mustafa ÇINAR <mustafa.cinar7@hotmail.com>
2021-05-02 22:10:26 +0300

    Initial commit

---
+++ NetCoreIOC.DataAccess/Repositories/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using NetCoreIOC.Entities.Models;
+using System.Collections.Generic;
+
+namespace NetCoreIOC.DataAccess.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly List<Product> _products;
+        public ProductRepository()
+        {
+            _products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Halley", Price = 5, Stock = 100 },
+                new Product { Id = 2, Name = "Marshmallow", Price = 10, Stock = 200 },
+                new Product { Id = 3, Name = "Cheetos", Price = 15, Stock = 300 },
+                new Product { Id = 4, Name = "Eticin", Price = 20, Stock = 50 },
+                new Product { Id = 5, Name = "Albeni", Price = 25, Stock = 60 },
+                new Product { Id = 6, Name = "Caramio", Price = 30, Stock = 70 }
+            };
+        }
+
+        public List<Product> GetProducts() => _products;
+    }
+}
